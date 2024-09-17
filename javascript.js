a027b30ccb8b0177119f1551bdd4f42420380e99
3antozz <106344848+3antozz@users.noreply.github.com>
2024-09-17 19:51:31 +0100

    basic overall layout, add and remove functions

--- javascript.js
+++ javascript.js
@@ -1,5 +1,9 @@
 const myLibrary = [];
-const container = document.querySelector(".container");
+const mainContainer = document.querySelector(".main");
+const addBookButton = document.querySelector(".add-book");
+
+addBookButton.addEventListener("click", addBookPrompt);
+
 
 
 function Books (title, author, pages, read) {
@@ -7,23 +11,59 @@ function Books (title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    addBookToLibrary(this);
 }
+
+function addBookPrompt () {
+    let bookTitle = prompt("Book title?");
+    let bookAuth = prompt("Book author?");
+    let bookPages = prompt("Book pages?");
+    let bookRead = prompt("Book read or not?");
+    let book = new Books(bookTitle, bookAuth, bookPages, bookRead);
+    addBookToLibrary(book);
+}
+
+
+
 
 function addBookToLibrary (book) {
-    return myLibrary.push(book);
+    myLibrary.push(book);
+    return displayBook(book);
 }
 
-function displayBook(array) {
-    array.forEach(book => {
-        let paragraph = document.createElement("p");
-        paragraph.textContent = book.title + book.author + book.pages + book.read;
-        container.appendChild(paragraph);
-    });
+function deleteBook (bookCard) {
+    mainContainer.removeChild(bookCard);
 }
 
-const book1 = new Books("Yacine", "lol", 22, "shit");
-const book2 = new Books("Alaa", "lol", 69, "shit");
-const book3 = new Books("Rafik", "lol", 420, "shit");
 
-displayBook(myLibrary);
+function displayBook(book) {
+    let cardDiv = document.createElement("div");
+    cardDiv.classList.add("card");
+    let textDiv = document.createElement("div");
+    textDiv.classList.add("text");
+    let bookTitle = document.createElement("h2");
+    bookTitle.textContent = book.title;
+    let bookAuthor = document.createElement("h3");
+    bookAuthor.textContent = book.author;
+    let bookPages = document.createElement("h3");
+    bookPages.textContent = book.pages;
+    let bookRead = document.createElement("h3");
+    bookRead.textContent = book.read;
+    let buttonsDiv = document.createElement("div");
+    buttonsDiv.classList.add("buttons");
+    let readToggle = document.createElement("button");
+    readToggle.setAttribute("type", "button");
+    readToggle.classList.add("toggle-read");
+    readToggle.textContent = "Toggle Read";
+    let removeButton = document.createElement("button");
+    removeButton.setAttribute("type", "button");
+    removeButton.classList.add("remove-book");
+    removeButton.textContent = "Remove";
+    mainContainer.appendChild(cardDiv);
+    cardDiv.appendChild(textDiv);
+    textDiv.append(bookTitle, bookAuthor, bookPages, bookRead);
+    buttonsDiv.append(readToggle, removeButton);
+    cardDiv.appendChild(buttonsDiv);
+
+    removeButton.addEventListener("click", () => deleteBook(cardDiv));
+}
+
